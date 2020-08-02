36de78f49ca36a3d1f3ccc010e1ef61ab70abc7a
Sulaiman <suleiman_hayek@yahoo.com>
2020-08-02 20:08:25 +0300

    change theme functionality in js and adding css dark-theme

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
 
 
 
+const toggleSwitch = document.querySelector('.toggle-btn input[type="checkbox"]');
 
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark'); //add this
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light'); //add this
+    }
+}
+
+
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
 
 // dropdown button functionality is disabled for now as its not neede in the design
 // instead we are using the slider button to change the theme color
